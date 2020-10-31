b20a0ed25ba0f88ad93ca9e76a2e391ad5bc97ae
unibreakfast <unibreakfast1@gmail.com>
2020-10-31 20:48:57 +0200

    first commit

---
+++ index.cjs
@@ -0,0 +1,37 @@
+const c = require('c4console')
+
+const http = require('http')
+
+
+module.exports = {c, run(options) {
+  const dev = typeof options.dev == 'boolean' ? options.dev : !process.env.PORT
+  const port = !dev ? process.env.PORT
+    : typeof options.port == 'number' ? options.port : 3000
+
+  if (dev) require = require('up2require')(require)
+
+  const handleRequest = require('./reqHan.cjs', dev)
+
+  const server = http.createServer(handleRequest)
+
+  server.listen(port, reportStart).on('error', c)
+
+  server.dev = dev
+
+  server.public = options.public || __dirname + '/public'
+
+  return server
+}}
+
+
+
+
+
+
+
+
+
+function reportStart() {
+  if (dev) c(`HTTP Server started at http://localhost:${port}`)
+  else c('===== server started =====')
+}
