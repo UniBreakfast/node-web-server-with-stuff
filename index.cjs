529478174f00f21798c3497290f888679d40f01b
unibreakfast <unibreakfast1@gmail.com>
2020-11-02 14:44:56 +0200

    start on the next port if provided one in use

--- index.cjs
+++ index.cjs
@@ -1,9 +1,13 @@
 const c = require('c4console')
+const up2 = require('up2require')
 
 const http = require('http')
 
 
-module.exports = {c, server: {run(options={}) {
+module.exports = {server: {run}, up2, c}
+
+
+function run(options={}) {
   const dev = this.dev =
     typeof options.dev == 'boolean' ? options.dev : !process.env.PORT
 
@@ -12,19 +16,29 @@ module.exports = {c, server: {run(options={}) {
 
   this.public = options.public || process.cwd() + '/public'
 
-  if (dev) require = require('up2require')(require)
+  if (dev) require = up2(require)
 
   const handleRequest = require('./requestHandler.cjs', dev)
 
   const server = http.createServer(handleRequest)
 
-  server.listen(port, () => reportStart(dev, port)).on('error', c)
-
   server.dev = dev
 
-  return server
-}}}
+  server.on('error',
+    err => err.code=='EADDRINUSE' ? start(server, port+1) : c(err))
 
+  start(server, port)
+
+  return server
+}
+
+
+function start(server, port) {
+  server.listen(port, () => {
+    if (server._connectionKey.endsWith(port)) reportStart(server.dev, port)
+    else c(`port ${port} is already in use`)
+  })
+}
 
 function reportStart(dev, port) {
   if (dev) c(`HTTP Server started at http://localhost:${port}`)
